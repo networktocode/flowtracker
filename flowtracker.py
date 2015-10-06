e1c6d8a460b8d067166c001898e0f5e9a182efa4
Michael Ben-Ami <mike.benami@gmail.com>
2015-10-06 16:05:24 -0400

    added timers and support for NXAPI

--- flowtracker.py
+++ flowtracker.py
@@ -1,13 +1,91 @@
+"""This script/module tracks the hop-by-hop path for a flow through Nexus switches,
+and gives interface statistics for each outgoing interface.
+"""
 import argparse
 import socket
 import sys
 import re
+import xml.etree.ElementTree as ET
+from datetime import datetime
 from netmiko import ConnectHandler
 from getpass import getpass
+from pycsco.nxos.device import Device as PYCSCO
+
+SSH_TYPE = 'ssh'
+PYCSCO_TYPE = 'pycsco'
 
 class HashError(Exception):
+    """Error raised when flow's hash can't be computed.
+    """
     def __init__(self, hash_output):
         self.hash_output = hash_output
+
+class Device(object):
+    """Base class for SSH (paramiko) and NXAPI (pycsco)
+       devices.
+    """
+    def __init__(self, device_type):
+        self.device_type = device_type
+
+    def get_instance(self, hostname, username, password):
+        """Return an instance of child class depending on device_type.
+        """
+        if self.device_type == SSH_TYPE:
+            return SshDevice(hostname, username, password)
+        elif self.device_type == PYCSCO_TYPE:
+            return PycscoDevice(hostname, username, password)
+
+    def show_command(self, cmd):
+        """Base method for returning output from a show command.
+        """
+        raise NotImplementedError
+
+    def disconnect(self):
+        """Base method for tearing down a connection.
+        """
+        raise NotImplementedError
+
+class SshDevice(Device):
+    """Class for communicating with switches over SSH.
+    """
+    def __init__(self, hostname, username, password):
+        try:
+            self.device = ConnectHandler(
+                device_type='cisco_nxos',
+                ip=hostname,
+                username=username,
+                password=password,
+                verbose=False
+            )
+        except:
+            print 'Error SSHing to device at {}'.format(
+                hostname)
+            print 'This may not be a Cisco device, or there may an authentication issue.'
+            sys.exit()
+
+    def show_command(self, cmd):
+        if not self.device.check_enable_mode():
+            self.device.enable()
+        return self.device.send_command(cmd)
+
+    def disconnect(self):
+        self.device.disconnect()
+
+class PycscoDevice(Device):
+    """Class for communicating with switches over NXAPI.
+    """
+    def __init__(self, hostname, username, password):
+        self.device = PYCSCO(username, password, hostname)
+
+    def show_command(self, cmd):
+        xml_rsp = self.device.show(cmd, text=True)
+        root = ET.fromstring(xml_rsp[1])
+        body = root.find('.//body').text
+        return body
+
+    def disconnect(self):
+        pass
+
 
 def convert_proto(proto):
     """Convert a common protocol name to a protocol number.
@@ -18,10 +96,10 @@ def convert_proto(proto):
 
     return convert_dict.get(proto, proto)
 
-def _get_raw_hash_info(device, src, dest, **kwargs):
-    if not device.check_enable_mode():
-        device.enable()
 
+def _get_raw_hash_info(device, src, dest, **kwargs):
+    """Get output of 'show routing hash' command.
+    """
     cmd_str = 'show routing hash {} {}'.format(
         src, dest)
 
@@ -41,8 +119,9 @@ def _get_raw_hash_info(device, src, dest, **kwargs):
     if vrf:
         cmd_str += ' {}'.format(vrf)
 
-    raw_hash_info = device.send_command(cmd_str)
+    raw_hash_info = device.show_command(cmd_str)
     return str(raw_hash_info)
+
 
 def get_hash_info(device, src, dest, **kwargs):
     """Get hash info from switch.
@@ -64,11 +143,10 @@ def get_hash_info(device, src, dest, **kwargs):
             out_if\n
             next_hop\n
     """
-    raw_info = _get_raw_hash_info(device, src, dest, **kwargs)
-    # TODO: get real info from switch
     hash_info = {}
+    raw_info = _get_raw_hash_info(device, src, dest, **kwargs)
 
-    match = re.search('Hash for VRF "(\w+)"', raw_info)
+    match = re.search('Hash for VRF\s+"?(\w+)"?', raw_info)
     if not match:
         raise HashError(raw_info)
 
@@ -94,12 +172,23 @@ def get_hash_info(device, src, dest, **kwargs):
 
     return hash_info
 
+
+def _get_raw_iface_stats(device, iface_name):
+    """Get 'show interface' output.
+    """
+    # if 'local' is in the name, don't attaempt show interface
+    if 'local' in iface_name:
+        return ''
+
+    raw_stats = device.show_command('show interface {}'.format(iface_name))
+    return str(raw_stats)
+
+
 def get_iface_stats(device, iface_name):
     """Parse interface error and counter info.
     """
-    raw_iface_info = _get_raw_iface_stats(device, iface_name)
-
     iface_stats = {}
+    raw_iface_info = _get_raw_iface_stats(device, iface_name)
 
     match = re.search('(\d+) interface resets', raw_iface_info)
     if match:
@@ -111,24 +200,13 @@ def get_iface_stats(device, iface_name):
 
     return iface_stats
 
-def _get_raw_iface_stats(device, iface_name):
-    """Get 'show interface' output.
-    """
-    if not device.check_enable_mode():
-        device.enable()
-
-    raw_stats = device.send_command('show interface {}'.format(iface_name))
-    return str(raw_stats)
 
 def get_mgmt(device, next_hop):
     """Try to glean the management IP address
-    of the next hop using 'sh cdp neighbors detail'
+    of the next hop using 'sh cdp neighbors detail'.
     """
-    if not device.check_enable_mode():
-        device.enable()
-
     mgmt_ip = None
-    cdp_raw = device.send_command('show cdp neighbors detail')
+    cdp_raw = device.show_command('show cdp neighbors detail')
     lines = cdp_raw.splitlines()
     neighbor_found = False
     mgmt_found = False
@@ -153,35 +231,31 @@ def get_mgmt(device, next_hop):
 
     return mgmt_ip
 
-def get_int_from_mac(device, mac):
-    if not device.check_enable_mode():
-        device.enable()
 
-    mac_table_raw = device.send_command('show mac address-table')
+def get_int_from_mac(device, mac):
+    """Get an interface, given a MAC address from the CAM table.
+    """
+    mac_table_raw = device.show_command('show mac address-table')
     match = re.search('{}\s+\w+\s+\d+\s+\w\s+\w\s+((\w|\d)+(\/\d+)?)'.format(mac), mac_table_raw)
     if match:
         return match.group(1)
 
 def get_mac_from_ip(device, ip):
-    if not device.check_enable_mode():
-        device.enable()
-
-    arp_table_raw = device.send_command('show ip arp')
+    """Get a MAC address, given an IP address from the ARP table.
+    """
+    arp_table_raw = device.show_command('show ip arp')
     match = re.search('{}\s+\d+:\d+:\d+\s+(((\w|\d)+\.){{2}}(\w|\d)+)'.format(ip), arp_table_raw)
     return match.group(1)
 
 def get_hostname(device):
     """Get the hostname of the device.
     """
-    if not device.check_enable_mode():
-        device.enable()
-    hostname = device.send_command('show hostname').strip()
+    hostname = device.show_command('show hostname').strip()
     return hostname
 
 def intro(args):
     """Intro message to script.
     """
-
     return """
 FLOW OVERVIEW:
 
@@ -202,7 +276,6 @@ VRF: {}
 def stringify(switch_ip, dest, hostname, hop_number, **hash_info):
     """Pretty print flow information.
     """
-
     return """
 #############################################################################
 
@@ -231,9 +304,10 @@ OUT_IF:
            hash_info.get('tx_counters') or 'N/A')
 
 def handle_args():
+    """Parse command line arguments, and prompt user if they are not given.
+    """
     parser = argparse.ArgumentParser()
 
-    # required args
     parser.add_argument('--src',
                         help='The source IP address or hostname of the flow.')
     parser.add_argument('--dest',
@@ -251,8 +325,12 @@ def handle_args():
                         help='The VRF of the flow.')
     parser.add_argument('--mode',
                         choices=['interactive', 'auto'],
-                        default='interactive')
-    parser.add_argument('--use_mgmt', action='store_true')
+                        default='interactive',
+                        help='Interactive mode prompts the user to continue at each hop.')
+    parser.add_argument('--use_mgmt',
+                        action='store_true',
+                        help='Use the Management IP address to connect to each hop.\n'\
+                             + 'Uses the NXAPI instead of SSH.')
 
     args = parser.parse_args()
     while not args.src:
@@ -261,10 +339,11 @@ def handle_args():
         args.dest = raw_input('Enter destination IP of the flow: ')
     while not args.proto:
         args.proto = raw_input('Enter IP protocol(tcp, udp, icmp, <number>): ')
+        args.proto = convert_proto(args.proto)
     while not args.target:
         args.target = raw_input('Enter IP or hostname of first switch to connect to: ')
     while not args.user:
-        args.user = raw_input('Enter the SSH username: ')
+        args.user = raw_input('Enter the SSH/NXAPI username: ')
     if args.use_mgmt is False:
         args.use_mgmt = None
     while args.use_mgmt is None:
@@ -276,52 +355,53 @@ def handle_args():
         else:
             args.use_mgmt = None
 
-    if args.mode == 'interactive':
+    if args.proto == '6' or args.proto == '17':
         args.src_port = args.src_port or raw_input('(Optional) TCP/UDP Source Port: ')
         args.dest_port = args.dest_port or raw_input('(Optional) TCP/UDP Destination Port: ')
-        args.vrf = args.vrf or raw_input('(Optional) VRF of the flow: ')
+
+    args.vrf = args.vrf or raw_input('(Optional) VRF of the flow: ')
 
     if bool(args.src_port) != bool(args.dest_port):
         print "--src_port and --dest_port must be supplied together."
         sys.exit()
 
-    args.pwd = getpass("SSH Password: ")
+    args.pwd = getpass("SSH/NXAPI Password: ")
     args.target = socket.gethostbyname(args.target)
     args.src = socket.gethostbyname(args.src)
     args.dest = socket.gethostbyname(args.dest)
-    args.proto = convert_proto(args.proto)
 
     return args
 
 if __name__ == "__main__":
+    # Handle arguments and print introduction
     args = handle_args()
-
-    username = 'cisco'
-    password = '!cisco123!'
-
     intro = intro(args)
     print intro
 
+    # Initialize loop conditional variables
     connect_ip = args.target
     out_if = 'unknown'
     hop_number = 1
+
+    # When the next hop is the destination,
+    # or when the next-hop is local to the switch,
+    # the loop stops.
     while args.target != args.dest\
             and 'local' not in out_if:
-        try:
-            device = ConnectHandler(
-                device_type='cisco_nxos',
-                ip=connect_ip,
-                username=args.user,
-                password=args.pwd,
-                verbose=False
-            )
-        except:
-            print 'Error SSHing to device at {}'.format(
-                connect_ip)
-            print 'This may not be a Cisco device, or there may an authentication issue.'
-            sys.exit()
 
-        # get stats from switch
+        # Used later to calculate elapsed time
+        starttime = datetime.now()
+
+        # Decide whether to connect over NXAPI or SSH
+        if args.use_mgmt:
+            device_type = PYCSCO_TYPE
+        else:
+            device_type = SSH_TYPE
+
+        device = Device(device_type).get_instance(connect_ip, args.user, args.pwd)
+
+
+        # Get details of flow's hash on current switch.
         try:
             hash_info = get_hash_info(device,
                                       src=args.src,
@@ -335,15 +415,17 @@ if __name__ == "__main__":
             print he.hash_output
             sys.exit()
 
+        # Get the outgoing interface and statistics
         out_if = hash_info.get('out_if')
         iface_stats = get_iface_stats(device, out_if)
-        hostname = get_hostname(device)
-
         hash_info.update(iface_stats)
 
+        # Print out information for the flow on current hop
+        hostname = get_hostname(device)
         out = stringify(args.target, args.dest, hostname, hop_number, **hash_info)
         print out
 
+        # Set up for next loop iteration
         args.target = hash_info.get('next_hop')
         out_if = hash_info.get('out_if')
 
@@ -355,6 +437,14 @@ if __name__ == "__main__":
         device.disconnect()
         hop_number += 1
 
+        # Calculate and print elapsed time
+        elapsed_time_delta = datetime.now() - starttime
+        fraction = elapsed_time_delta.microseconds / 1000000.0
+        elapsed_time = elapsed_time_delta.seconds + fraction
+
+        print "Elapsed Time for Hop: " + str(elapsed_time) + " seconds."
+
+        # In interactive mode, have the user hit Enter to continue
         if args.mode == 'interactive':
             loop = 'a'
             while loop:
