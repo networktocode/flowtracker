0bc1a6a82ef852dc4da42dcde115d87b1c61575c
Michael Ben-Ami <mike.benami@gmail.com>
2015-10-07 13:45:25 -0400

    changed version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(name='flowtracker',
-      version='0.0.1',
+      version='0.0.2',
       description='Track multi-pathed flows through Cisco Nexus switches.',
       packages=[],
       install_requires=['netmiko', 'pycsco'],
